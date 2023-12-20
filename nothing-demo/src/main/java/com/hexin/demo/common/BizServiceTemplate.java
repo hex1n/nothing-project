2a7b5ccb1ff55e49c9f0009868e4dc6bdeb748a8
hex1n <976559968@qq.com>
2023-12-20 19:58:24 +0800

    commit

--- nothing-demo/src/main/java/com/hexin/demo/common/BizServiceTemplate.java
+++ nothing-demo/src/main/java/com/hexin/demo/common/BizServiceTemplate.java
@@ -1,8 +1,9 @@
 package com.hexin.demo.common;
 
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.Maps;
-import com.hexin.demo.entity.WebResponse;
 import com.hexin.demo.constant.ErrorCode;
+import com.hexin.demo.entity.WebResponse;
 import com.hexin.demo.exception.BizException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.text.MessageFormat;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author hex1n
@@ -21,22 +23,30 @@ import java.util.concurrent.ConcurrentMap;
 public class BizServiceTemplate {
 
     public <P, R> WebResponse<R> process(P param, String methodName, ServiceExecutor<P, R> serviceExecutor) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
         String requestMessage = MessageFormat.format("[{0}]--->methodName:[{1}]#requestParam:{2}", this.getClass().getSimpleName(), methodName, param);
         log.info(requestMessage);
         ConcurrentMap<Object, Object> context = Maps.newConcurrentMap();
         try {
             serviceExecutor.paramCheck(param, context);
             R result = serviceExecutor.process(param, context);
+            long elapsed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+            String msg = MessageFormat.format("业务处理 [{0}]--->methodName:[{1}] 耗时:[{2}ms]", this.getClass().getSimpleName(), methodName, elapsed);
+            log.info(msg);
             return WebResponse.buildSuccessWithData(result);
         } catch (BizException bizException) {
-            String msg = MessageFormat.format("业务异常 [{0}]--->methodName:[{1}]", this.getClass().getSimpleName(), methodName);
+            long elapsed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+            String msg = MessageFormat.format("业务处理异常 [{0}]--->methodName:[{1}] 耗时:[{2}ms]", this.getClass().getSimpleName(), methodName, elapsed);
             log.error(msg, bizException);
             return WebResponse.buildFailResult(bizException.getErrorCode(), bizException.getMessage());
         } catch (Exception e) {
-            String msg = MessageFormat.format("系统异常[{0}]--->methodName:[{1}]", this.getClass().getSimpleName(), methodName);
+            long elapsed = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+            String msg = MessageFormat.format("系统异常[{0}]--->methodName:[{1}] 耗时:[{2}ms]", this.getClass().getSimpleName(), methodName, elapsed);
             log.error(msg, e);
             return WebResponse.buildFailResult(ErrorCode.INTERNAL_SERVER_ERROR.getCode(), ErrorCode.INTERNAL_SERVER_ERROR.getDesc());
         }
+
+
     }
 
     public interface ServiceExecutor<P, R> {
